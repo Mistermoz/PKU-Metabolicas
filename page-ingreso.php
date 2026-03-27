20213ed4dc705d60c0a83555f0749516ae68c271
claudio.donoso <claudiodonosoc@gmail.com>
2026-03-27 00:04:06 -0300

    update page ingresos

--- page-ingreso.php
+++ page-ingreso.php
@@ -32,9 +32,12 @@
 			<label class="nom_campo nom_campo--nom" id="nom">Paciente</label>
 			<label class="nom_campo nom_campo--rut_nom" id="rut_nom">Rut/Nombre</label>
 			<label class="nom_campo nom_campo--flec" id="flec">F. Lectura</label>
-			<label class="nom_campo nom_campo--fen" id="fen">Fenil</label>
-			<label class="nom_campo nom_campo--leu" id="leu">Leu</label>
-			<label class="nom_campo nom_campo--tir" id="tir">Tir</label>
+			<label class="nom_campo nom_campo--fen PKU amin" id="fen">Fenil</label>
+			<label class="nom_campo nom_campo--leu MSUD amin" id="leu">Leu</label>
+			<label class="nom_campo nom_campo--tir amin" id="tir">Tir</label>
+			<label class="nom_campo nom_campo--val MSUD amin" id="val">Val</label>
+			<label class="nom_campo nom_campo--iso MSUD amin" id="iso">Iso</label>
+			<label class="nom_campo nom_campo--allo MSUD amin" id="allo">Allo</label>
 			<label class="nom_campo nom_campo--fmues" id="fmues">F. Muestra</label>
 		  </div>
 		  <div class="box_inputs">
@@ -42,7 +45,7 @@
 			  <div class="numero nom_campo--nro">
 			  	<label class="nro_paciente">1.</label><input type="checkbox" class="nro" name="nro1" value=""/>
 			  </div>
-			  <div id="box_paciente1 nom_campo--nom" class="box_paciente box_paciente1">
+			  <div id="box_paciente1" class="box_paciente box_paciente1 nom_campo--nom">
 				<label class="elige_paciente">Rut</label><input type="radio" id="rut" name="paciente" value="0" rel="1"/>
 				<label class="elige_paciente">Nombre</label><input type="radio" id="nombre" name="paciente" value="1" rel="1"/>
 			  </div>
@@ -50,9 +53,12 @@
 			  	<input type="text" class="input_rut_nom" name="rut_nom1" value="" readonly="readonly"/>
 			  </div>
 			  <input type="text" name="flectura1" class="flectura fecha nom_campo--flec" value="" placeholder="Ej:dd-mm-aaaa" />
-			  <input type="text" name="fenil1" class="fenil nom_campo--fen" value="" placeholder="Fenil" />
-			  <input type="text" name="leu1" class="leu nom_campo--leu" value="" placeholder="Leu" />
-			  <input type="text" name="tiro1" class="tiro nom_campo--tir" value="" placeholder="Tir" />
+			  <input type="text" name="fenil1" class="fenil nom_campo--fen PKU amin" value="" placeholder="Fenil" />
+			  <input type="text" name="leu1" class="leu nom_campo--leu MSUD amin" value="" placeholder="Leu" />
+			  <input type="text" name="tiro1" class="tiro nom_campo--tir amin" value="" placeholder="Tir" />
+			  <input type="text" name="val1" class="val nom_campo--val MSUD amin" value="" placeholder="Val" />
+			  <input type="text" name="iso1" class="iso nom_campo--iso MSUD amin" value="" placeholder="Iso" />
+			  <input type="text" name="allo1" class="allo nom_campo--allo MSUD amin" value="" placeholder="Allo" />
 			  <input type="text" name="fmuestra1" class="fmuestra fecha nom_campo--fmues" value="" placeholder="Ej:dd-mm-aaaa" />
 			</div>
 		  </div>
@@ -74,15 +80,11 @@
 		/*** Elección de Enfermedad Fenil o Leu ****/
 		jQuery("input[name='enfermedad']").bind('click',function(){
 			if(jQuery(this).val() == '1'){
-				jQuery('.leu').css('display','block');
-				jQuery('#leu').css('display','block');
-				jQuery('#fen').css('display','none');
-				jQuery('.fenil').css('display','none');
-			}else{
-				jQuery('.leu').css('display','none');
-				jQuery('#leu').css('display','none');
-				jQuery('#fen').css('display','block');
-				jQuery('.fenil').css('display','block');
+				jQuery('.MSUD').css('display','block');
+				jQuery('.PKU').css('display','none');
+			} else {
+				jQuery('.MSUD').css('display','none');
+				jQuery('.PKU').css('display','block');
 			}
 		});
 		/*** Elección de Paciente ****/
@@ -153,6 +155,9 @@
 			var fenil = new Array();
 			var tir = new Array();
 			var leu = new Array();
+			var val = new Array();
+			var iso = new Array();
+			var allo = new Array();
 			var fmuestra = new Array();
 			var c = jQuery(".box_inputs > .registro").length;
 			for(j=1; j<=c; j++) {
@@ -185,14 +190,17 @@
 				fenil[j-1] = jQuery('input[name="fenil'+j+'"]').val();
 				tir[j-1] =jQuery('input[name="tiro'+j+'"]').val();
 				leu[j-1] = jQuery('input[name="leu'+j+'"]').val();
+				val[j-1] = jQuery('input[name="val'+j+'"]').val();
+				iso[j-1] = jQuery('input[name="iso'+j+'"]').val();
+				allo[j-1] = jQuery('input[name="allo'+j+'"]').val();
 				fmuestra[j-1] =jQuery('input[name="fmuestra'+j+'"]').val();
 			}
-			jQuery("#log").text('leu' + leu);
+			//jQuery("#log").text('leu' + leu);
 			jQuery.ajax({
 				type: "POST",
 				cache: true,
 				data:
-				{'action':'ingreso_pacientes', 'rut_paciente': rut_paciente, 'nom_paciente' : nom_paciente, 'flectura' : flectura, 'fenil' : fenil, 'tir' : tir, 'leu' :leu, 'fmuestra' : fmuestra},
+				{'action':'ingreso_pacientes', 'rut_paciente': rut_paciente, 'nom_paciente' : nom_paciente, 'flectura' : flectura, 'fenil' : fenil, 'tir' : tir, 'leu' : leu, 'val' : val, 'iso' : iso, 'allo' : allo, 'fmuestra' : fmuestra},
 				url: url_ajax,
 				dataType: "json",
 				beforeSend: function () {
@@ -209,7 +217,24 @@
 		function agregar_paciente(){
 			var c = jQuery(".box_inputs > .registro").length;
 			c = c+1;
-			jQuery('.box_inputs').append('<div id="registro'+c+'" class="registro registro'+c+'" rel="'+c+'"><div class="numero"><label class="nro_paciente">'+c+'.</label><input type="checkbox" class="nro" name="nro'+c+'" value=""/></div><div id="box_paciente'+c+'" class="box_paciente box_paciente'+c+'"><label class="elige_paciente">Rut</label><input type="radio" id="rut" name="paciente" value="0" rel="'+c+'"/><label class="elige_paciente">Nombre</label><input type="radio" id="nombre" name="paciente" value="1" rel="'+c+'"/></div><div class="rut nom_campo--rut_nom"><input type="text" class="input_rut_nom" name="rut_nom'+c+'" value=""/></div><input type="text" name="flectura'+c+'" class="flectura fecha" value="" placeholder="Ej:dd-mm-aaaa" /><input type="text" name="fenil'+c+'" class="fenil" value="" placeholder="Fenil" /><input type="text" name="leu'+c+'" class="leu" value="" placeholder="Leu" /><input type="text" name="tiro'+c+'" class="tiro" value="" placeholder="Tir" /><input type="text" name="fmuestra'+c+'" class="fmuestra fecha" value="" placeholder="Ej:dd-mm-aaaa" /></div>');
+			jQuery('.box_inputs').append('<div id="registro'+c+'" class="registro registro'+c+'" rel="'+c+'">'+
+			'<div class="numero nom_campo--nro">'+
+			'<label class="nro_paciente">'+c+'.</label>'+
+			'<input type="checkbox" class="nro" name="nro'+c+'" value=""/></div>'+
+			'<div id="box_paciente'+c+'" class="box_paciente box_paciente'+c+' nom_campo--nom">'+
+			'<label class="elige_paciente">Rut</label>'+
+			'<input type="radio" id="rut" name="paciente" value="0" rel="'+c+'"/>'+
+			'<label class="elige_paciente">Nombre</label>'+
+			'<input type="radio" id="nombre" name="paciente" value="1" rel="'+c+'"/></div>'+
+			'<div class="rut nom_campo--rut_nom"><input type="text" class="input_rut_nom" name="rut_nom'+c+'" value=""/></div>'+
+			'<input type="text" name="flectura'+c+'" class="flectura fecha nom_campo--flec" value="" placeholder="Ej:dd-mm-aaaa" />'+
+			'<input type="text" name="fenil'+c+'" class="fenil PKU amin" value="" placeholder="Fenil" />'+
+			'<input type="text" name="leu'+c+'" class="leu MSUD amin" value="" placeholder="Leu" />'+
+			'<input type="text" name="tiro'+c+'" class="tiro amin" value="" placeholder="Tir" />'+
+			'<input type="text" name="val'+c+'" class="val MSUD amin" value="" placeholder="Val" />'+
+			'<input type="text" name="iso'+c+'" class="iso MSUD amin" value="" placeholder="Iso" />'+
+			'<input type="text" name="allo'+c+'" class="allo MSUD amin" value="" placeholder="Allo" />'+
+			'<input type="text" name="fmuestra'+c+'" class="fmuestra fecha nom_campo--fmues" value="" placeholder="Ej:dd-mm-aaaa" /></div>');
 			jQuery( ".fecha" ).datepicker({
 			    changeMonth: true,
 			    changeYear: true,
@@ -272,6 +297,9 @@
 						jQuery('#registro'+i+'').find('.fenil').attr('name', 'fenil'+i+'');
 						jQuery('#registro'+i+'').find('.tiro').attr('name', 'tiro'+i+'');
 						jQuery('#registro'+i+'').find('.leu').attr('name', 'leu'+i+'');
+						jQuery('#registro'+i+'').find('.val').attr('name', 'val'+i+'');
+						jQuery('#registro'+i+'').find('.iso').attr('name', 'iso'+i+'');
+						jQuery('#registro'+i+'').find('.allo').attr('name', 'allo'+i+'');
 						jQuery('#registro'+i+'').find('.fmuestra').attr('name', 'fmuestra'+i+'');
 						jQuery('#registro'+i+'').find('.input_rut').attr('name', 'rut'+i+'');
 					}
